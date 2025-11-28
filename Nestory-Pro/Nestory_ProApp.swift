68cbb0fdbadc603084a6add0604f4e435c9aebe2
drunkonjava <griffinradcliffe@gmail.com>
2025-11-28 09:03:28 -0500

    Initial Commit

---
+++ Nestory-Pro/Nestory_ProApp.swift
@@ -0,0 +1,32 @@
+//
+//  Nestory_ProApp.swift
+//  Nestory-Pro
+//
+//  Created by Griffin on 11/28/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Nestory_ProApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Nestory-ProTests/Nestory_ProTests.swift
@@ -0,0 +1,17 @@
+//
+//  Nestory_ProTests.swift
+//  Nestory-ProTests
+//
+//  Created by Griffin on 11/28/25.
+//
+
+import Testing
+@testable import Nestory_Pro
+
+struct Nestory_ProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
